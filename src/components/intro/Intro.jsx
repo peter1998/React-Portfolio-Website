33ffc861cc5abcfbdcc602b068dd1b26961be961
Peter Matov <pmatov@gmail.com>
2022-11-09 22:40:29 +0200

    [C] Text typing animation in intro page.

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,20 @@
-import React from "react";
-
 import "./intro.scss";
+import { init } from "ityped";
+import Typewriter from "typewriter-effect";
+import { useEffect, useRef } from "react";
 
-export default function intro() {
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["React Developer", "Native Developer", "GraphQL back-end"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -15,7 +27,7 @@ export default function intro() {
           <h2>Hi There, I'm</h2>
           <h1>Peter Matov</h1>
           <h3>
-            Front-End<span>React Developer</span>
+            Front-End <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
