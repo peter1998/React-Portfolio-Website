290bbd644dde8d675765d4277e5dbe9c549d7ef7
Peter Matov <pmatov@gmail.com>
2022-11-09 10:52:24 +0200

    [C] React Animated hamburger menu.

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import "./topbar.scss";
 import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
@@ -21,7 +21,13 @@ export default function Topbar() {
           </div>
         </div>
 
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
