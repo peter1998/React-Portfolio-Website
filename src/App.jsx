635416cc4375e23a51403bb2ec6ccdd04320e003
Peter Matov <pmatov@gmail.com>
2022-11-09 18:56:46 +0200

    [C] React Sidebar menu for portfolio.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+import { useState } from "react";
+
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 
 import "./app.scss";
-
-import { useState } from "react";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(true);
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
