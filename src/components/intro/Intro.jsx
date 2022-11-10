04cb5d8cff010afb8034913d8d6631003c58ff1c
Peter Matov <pmatov@gmail.com>
2022-11-10 10:58:49 +0200

    [C] Portfolio classes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -11,7 +11,7 @@ export default function Intro() {
       showCursor: true,
       backDelay: 1500,
       backSpeed: 60,
-      strings: ["React Developer", "Native Developer", "GraphQL back-end"],
+      strings: ["React Developer", "Native Developer", "GraphQL Initializer"],
     });
   }, []);
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,7 +5,40 @@ import "./portfolio.scss";
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      Portfolio
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   );
 }
